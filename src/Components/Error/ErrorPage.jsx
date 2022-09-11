6d3f677c321bcff8bd85d77e42bb808157ed1d1f
Nagaraj Velusamy <nagaraj.velusamy@nutechnologyinc.com>
2022-09-11 20:32:53 +0530

    404 image update

--- src/Components/Error/ErrorPage.jsx
+++ src/Components/Error/ErrorPage.jsx
@@ -1,18 +1,19 @@
-import Error from "../../assets/images/error-404.png"
-import "./Error.scss";
+// import Error from "../../assets/images/error-404.png"
+import PageNotFound from "../../assets/images/pagenotfound.svg";
+import "./ErrorPage.scss";
 
 const ErrorPage = () => {
   return (
     <div className="error-page-wrapper">
-        <div className="error-page-content">
-            <div className="error-page-content-img">
-                <img src={Error} />
-            </div>
-            <div className="error-page-content-text">
-                Oops! Page Not Found
-            </div>
-            <button className="error-page-content-btn">Go To Home</button>
+      <div className="error-page-content">
+        <div className="error-page-content-img">
+          <img src={PageNotFound} alt="Page Not Found" />
         </div>
+        <div className="error-page-content-text">
+          Oops! Page Not Found
+        </div>
+        <button className="error-page-content-btn" type="button">Go To Home</button>
+      </div>
     </div>
   )
 }
